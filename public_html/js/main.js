6c2a0c257842760030da2b15cb0b204edae3aa9f
Luka <luka.rako993@gmail.com>
2024-01-16 16:26:37 +0100

    contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -51,8 +51,50 @@ $(document).ready(function () {
                 }
 
         );
+    };
+   
+   
+    if($('.contact-form'.length > 0)){
+        
+        $('.contact-form').validate({
+           
+            highlight: function(element){
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function(element){
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                    email: true
+                },              
+                message: {
+                    required: true,
+                    minlength: 10
+                }               
+            },
+            messages : {
+                name : {
+                    required: 'The Name* field is required!',
+                    minlength: 'Minimum length is 3 characters!'
+                },
+                email : {
+                    required: 'The Email* field is required!',
+                    email: 'Please enter an valid email adress!'
+                }
+            },
+            errorElement : 'p',
+            errorPlacement : function(error, element){
+                error.appendTo(element.closest('.form-group').find('.error-msg'));
+            }
+        });
+        
     }
-    ;
 
 
 
